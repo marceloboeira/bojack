7d9f8db57629febebebbab0d632946332b24f1a0
Marcelo Boeira <me@marceloboeira.com>
2016-07-10 01:54:15 -0300

    feature: Create basic 'pong' TCP Server
    On start it creates a TCP Server on port 5000 and answers to each
    request with 'pong', the first command will be 'ping'.

--- src/bojack.cr
+++ src/bojack.cr
@@ -1,3 +1,23 @@
 require "./bojack/*"
+require "socket"
 
-module BoJack; end
+module BoJack
+  class Server
+    def self.start
+      server = TCPServer.new("localhost", 5000)
+      server.recv_buffer_size = 4096
+
+      loop do
+        socket = server.accept
+        if socket
+          spawn do
+            loop do
+              request = socket.gets
+              socket.puts("pong") if request
+            end
+          end
+        end
+      end
+    end
+  end
+end
