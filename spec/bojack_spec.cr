45db098125a0521a96661144cd5f4bbc2e6286a7
Marcelo Boeira <me@marceloboeira.com>
2016-07-10 10:25:10 -0300

    test: Add basic spec
    - Test connection
    - Test pong

--- spec/bojack_spec.cr
+++ spec/bojack_spec.cr
@@ -1,4 +1,29 @@
 require "./spec_helper"
 
-describe BoJack do
+
+describe BoJack::Server do
+  before do
+    Thread.new { BoJack::Server.start }
+  end
+
+  context "on connection" do
+    it "sends a hello message" do
+      TCPSocket.open("localhost", 5000) do |socket|
+        # ensures block execution
+        expect(true).to eq(true)
+      end
+    end
+  end
+
+  context "when ping" do
+    it "pongs" do
+      TCPSocket.open("localhost", 5000) do |socket|
+        socket.puts("ping")
+
+        buffer = socket.gets
+
+        expect(buffer).to eq("pong\n")
+      end
+    end
+  end
 end
