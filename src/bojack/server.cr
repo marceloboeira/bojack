a8318213a555c43df46b9d322047379c027e9853
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-07-15 10:17:37 -0300

    fix: Break the reading loop when request is nil

--- src/bojack/server.cr
+++ src/bojack/server.cr
@@ -17,47 +17,48 @@ module BoJack
         if socket = server.accept
           spawn do
             loop do
-              if request = socket.gets
-                request = request.split(" ").map{|item| item.strip }
-                command = request[0]
+              request = socket.gets
+              break unless request
 
-                if command == "ping"
-                  socket.puts("pong")
-                elsif command == "set"
-                  key = request[1]
-                  value = request[2]
+              request = request.split(" ").map { |item| item.strip }
+              command = request[0]
 
-                  memory.write(key, value)
+              if command == "ping"
+                socket.puts("pong")
+              elsif command == "set"
+                key = request[1]
+                value = request[2]
 
+                memory.write(key, value)
+
+                socket.puts(value)
+              elsif command == "get"
+                key = request[1]
+
+                begin
+                  value = memory.read(key)
                   socket.puts(value)
-                elsif command == "get"
-                  key = request[1]
-
-                  begin
-                    value = memory.read(key)
-                    socket.puts(value)
-                  rescue
-                    socket.puts("error: '#{key}' is not a valid key")
-                  end
-                elsif command == "delete"
-                  key = request[1]
-
-                  begin
-                    value = memory.delete(key)
-                    socket.puts(value)
-                  rescue
-                    socket.puts("error: '#{key}' is not a valid key")
-                  end
-                elsif command == "size"
-                  socket.puts(memory.size)
-                elsif command == "close"
-                  socket.puts("closing...")
-
-                  socket.close
-                  break
-                else
-                  socket.puts("error: '#{command}' is not a valid command")
+                rescue
+                  socket.puts("error: '#{key}' is not a valid key")
                 end
+              elsif command == "delete"
+                key = request[1]
+
+                begin
+                  value = memory.delete(key)
+                  socket.puts(value)
+                rescue
+                  socket.puts("error: '#{key}' is not a valid key")
+                end
+              elsif command == "size"
+                socket.puts(memory.size)
+              elsif command == "close"
+                socket.puts("closing...")
+
+                socket.close
+                break
+              else
+                socket.puts("error: '#{command}' is not a valid command")
               end
             end
           end
