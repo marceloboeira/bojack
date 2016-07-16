8210ab60c0156c90072cf541a048a4d423b6a3a9
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-07-16 20:39:31 -0300

    tests: adding commands tests

---
+++ spec/bojack/command_spec.cr
@@ -0,0 +1,36 @@
+require "../spec_helper"
+require "../../src/bojack/command"
+require "../../src/bojack/commands/*"
+
+
+describe "BoJack::Command" do
+  context "when receive set" do
+    it "returns a Set command" do
+      (Bojack::Command.from("set")).should be_a(Bojack::Commands::Set)
+    end
+  end
+
+  context "when receive get" do
+    it "returns a Get command" do
+      (Bojack::Command.from("get")).should be_a(Bojack::Commands::Get)
+    end
+  end
+
+  context "when receive delete" do
+    it "returns a Delete command" do
+      (Bojack::Command.from("delete")).should be_a(Bojack::Commands::Delete)
+    end
+  end
+
+  context "when receive size" do
+    it "returns a Size command" do
+      (Bojack::Command.from("size")).should be_a(Bojack::Commands::Size)
+    end
+  end
+
+  context "when receive unkowncommand" do
+    it "returns a nil" do
+      (Bojack::Command.from("unknowncommand")).should be_nil
+    end
+  end
+end
