b39c1f37cb4ed947d26a07297cc2bf5ad262d499
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-07-17 11:03:37 -0300

    refactor: using abstract class

--- src/bojack/commands/command.cr
+++ src/bojack/commands/command.cr
@@ -4,16 +4,15 @@ module Bojack
   module Commands
     # Represents a Bojack command
     #
-    # It is a interface that each command
-    # must too implement.
-    # 
-    # @param memory [Bojack::Memory] to be managed.
-    # @param key [String] for memory retrieve
-    # @param value [String] for memory changes
-    class Command
-      def execute(memory, key : String?, value : String?)
-        raise "Execution not implemented."
-      end
+    abstract class Command
+      # Execute method
+      #
+      # Implements the process for command.
+      #
+      # @param memory [Bojack::Memory] to be managed.
+      # @param key [String] for memory retrieve.
+      # @param value [String] for memory changes.
+      abstract def execute(memory, key : String?, value : String?) : String
     end
   end
 end
