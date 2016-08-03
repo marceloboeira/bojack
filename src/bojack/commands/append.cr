a8d34037f479f73d7476706f769cdc13f94ff951
Marcelo Boeira <me@marceloboeira.com>
2016-08-03 00:22:04 -0300

    fix: Add append command keyword definition

--- src/bojack/commands/append.cr
+++ src/bojack/commands/append.cr
@@ -3,6 +3,8 @@ require "./command"
 module Bojack
   module Commands
     class Append < Command
+      self.keyword = "append"
+
       def execute(memory, key, value)
         memory.append(key, value)
       rescue ex
