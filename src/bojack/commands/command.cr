ac3d22d117d676012b727b37b8ff9e554a35b2a8
Marcelo Boeira <me@marceloboeira.com>
2016-08-15 21:48:04 +0200

    refactor: Remove legacy abstract references to keyword

--- src/bojack/commands/command.cr
+++ src/bojack/commands/command.cr
@@ -3,17 +3,7 @@ require "../memory"
 module BoJack
   module Commands
     abstract class Command
-      @@keyword : String = "none"
-
       abstract def execute(memory, params : Array(String)) : String | Array(String)
-
-      private def self.keyword=(value : String)
-        @@keyword = value
-      end
-
-      def self.keyword
-        @@keyword
-      end
     end
   end
 end
