a5aa5666d48a2d6634e39891b5e4456548a5ff8f
Marcelo Boeira <me@marceloboeira.com>
2016-08-15 21:30:40 +0200

    feature: Add basic logger

--- src/bojack/server.cr
+++ src/bojack/server.cr
@@ -1,4 +1,5 @@
 require "socket"
+require "logger"
 require "./memory"
 require "./params"
 require "./command"
@@ -8,19 +9,27 @@ module BoJack
     @hostname : String
     @port : Int8 | Int16 | Int32 | Int64
 
-    def initialize(@hostname = "127.0.0.1", @port = 5000); end
+    def initialize(@hostname = "127.0.0.1", @port = 5000)
+      @logger = Logger.new(STDOUT)
+    end
 
     def start
       server = TCPServer.new(@hostname, @port)
       server.recv_buffer_size = 4096
       memory = BoJack::Memory(String, Array(String)).new
 
+      @logger.info("Server started on #{@hostname}:#{@port}")
+
       loop do
         if socket = server.accept
+          @logger.info("#{socket.remote_address} connected")
+
           spawn do
             loop do
               request = socket.gets
               break unless request
+
+              @logger.info("#{socket.remote_address} requested: #{request.dump}")
 
               params = BoJack::Params.from(request)
               command = BoJack::Command.from(params[:command])
@@ -30,6 +39,8 @@ module BoJack
                 socket.puts(response)
               elsif params[:command] == "close"
                 socket.puts("closing...")
+                @logger.info("#{socket.remote_address} closed the connection")
+
                 socket.close
                 break
               else
