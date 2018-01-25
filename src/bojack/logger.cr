a3ed3b36a104b5ce0768bcb5daef74d03fdbee27
Hugo Abonizio <hugo_abonizio@hotmail.com>
2018-01-25 09:59:06 -0200

    feature: Format logger datetime as UTC

--- src/bojack/logger.cr
+++ src/bojack/logger.cr
@@ -4,7 +4,7 @@ module BoJack
   class Logger < ::Logger
     @@instance : BoJack::Logger = BoJack::Logger.new(STDOUT)
 
-    def self.build(io : (IO | String) = STDOUT, level : Int32 = 1, hostname : String = "127.0.0.1",  port : Int32 = 5000)
+    def self.build(io : (IO | String) = STDOUT, level : Int32 = 1, hostname : String = "127.0.0.1", port : Int32 = 5000)
       if io.is_a?(String)
         basename = File.basename(io, ".log")
         path = File.dirname(io)
@@ -16,6 +16,7 @@ module BoJack
       @@instance = BoJack::Logger.new(io)
       @@instance.level = BoJack::Logger::Severity.new(level)
       @@instance.formatter = BoJack::Logger::Formatter.new do |severity, datetime, progname, message, io|
+        datetime = datetime.to_utc.to_s("%Y-%m-%d %H:%M:%S.%L")
         io << "[bojack][#{hostname}:#{port}][#{datetime}][#{severity}] #{message}"
       end
     end
