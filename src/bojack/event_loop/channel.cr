1f93854ab95b9f75f56c5b922f573b9d7e999dbe
Marcelo Boeira <me@marceloboeira.com>
2018-01-31 00:15:14 +0100

    refactor: Use request instead of action
    the name binds to the type other than a new concept

--- src/bojack/event_loop/channel.cr
+++ src/bojack/event_loop/channel.cr
@@ -6,8 +6,8 @@ module BoJack
       def start
         spawn do
           loop do
-            if action = @channel.receive
-              action.perform
+            if request = @channel.receive
+              request.perform
             end
           end
         end
